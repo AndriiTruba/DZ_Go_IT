c02316e33c673fe93d23131b9f1bae3e8a366165
Andrii Truba <truba.a@icloud.com>
2022-11-20 01:02:12 +0200

    did dz module 7

--- module_07/main_client.py
+++ module_07/main_client.py
@@ -1,26 +1,58 @@
 import socket
+from time import sleep
 
 TCP_IP = 'localhost'
 TCP_PORT = 8080
 
 
-def run_client(ip: str, port: int):
+def main(host, port):
+    while True:
+        message_from_server = run_client(host, port)
+        if message_from_server != "Oк!":
+            print(f'Data transfer completed')
+            break
+        message_from_client = run_server(host, port)
+        if message_from_client == 'exit':
+            print(f'Data transfer completed')
+            break
+
+
+def run_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(2)
+    new_sock, address = server_socket.accept()
+    print(f"Server socket run ({host} : {port})")
+    try:
+        message_from_client = new_sock.recv(256).decode()
+        if message_from_client == 'exit':
+            return message_from_client
+        print(f"From client: {message_from_client}")
+        print(f"To exit enter: 'exit'")
+        message = "Oк!"
+        new_sock.send(message.encode())
+    except KeyboardInterrupt:
+        print(f'Destroy server')
+    finally:
+        server_socket.close()
+
+
+def run_client(host: str, port: int):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        server = ip, port
-        client_socket.connect(server)
-        print(f"Client socket run ({ip} : {port})")
-        message = input('>> ').lower().strip()
-        if len(message) > 0:
-            while message != 'exit':
+        try:
+            server = host, port
+            client_socket.connect(server)
+            print(f"Client socket run ({host} : {port})")
+            while True:
+                message = input('>> ').lower().strip()
                 client_socket.send(message.encode())
                 message_from_server = client_socket.recv(256).decode()
-                print(f"Received notification: {message_from_server}")
-                print(f"To exit enter: 'exit'")
-                message = input('>> ').lower().strip()
-                if len(message) == 0:
-                    break
-    print(f'Data transfer completed')
+                print(f"From server: {message_from_server}")
+                if message_from_server == "Oк!":
+                    return message_from_server
+        except ConnectionRefusedError:
+            sleep(0.5)
 
 
 if __name__ == '__main__':
-    run_client(TCP_IP, TCP_PORT)
+    main(TCP_IP, TCP_PORT)
--- module_07/main_server.py
+++ module_07/main_server.py
@@ -1,29 +1,59 @@
 import socket
+from time import sleep
 
 TCP_IP = 'localhost'
 TCP_PORT = 8080
 
 
-def run_server(ip, port):
+def main(host, port):
+    while True:
+        message_from_client = run_server(host, port)
+        if message_from_client == 'exit':
+            print(f'Data transfer completed')
+            break
+        message_from_server = run_client(host, port)
+        if message_from_server:
+            print(f'Data transfer completed')
+            break
+
+
+def run_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((ip, port))
+    server_socket.bind((host, port))
     server_socket.listen(2)
-    print(f'Start echo server {server_socket.getsockname()}')
     new_sock, address = server_socket.accept()
+    print(f"Server socket run ({host} : {port})")
     try:
-        while True:
-            message_from_client = new_sock.recv(256).decode()
-            if not message_from_client:
-                print(message_from_client, 'Data transfer completed')
-                break
-            print(f"Received notification: {message_from_client}")
-            message = "Oк!"
-            new_sock.send(message.encode())
+        message_from_client = new_sock.recv(256).decode()
+        if message_from_client == 'exit':
+            return message_from_client
+        print(f"From client: {message_from_client}")
+        print(f"To exit enter: 'exit'")
+        message = "Oк!"
+        new_sock.send(message.encode())
+
     except KeyboardInterrupt:
         print(f'Destroy server')
     finally:
         server_socket.close()
 
 
+def run_client(host: str, port: int):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        try:
+            server = host, port
+            client_socket.connect(server)
+            print(f"Client socket run ({host} : {port})")
+            while True:
+                message = input('>> ').lower().strip()
+                client_socket.send(message.encode())
+                message_from_server = client_socket.recv(256).decode()
+                print(f"From server: {message_from_server}")
+                if message_from_server == "Oк!":
+                    return message_from_server
+        except ConnectionRefusedError:
+            sleep(0.5)
+
+
 if __name__ == '__main__':
-    run_server(TCP_IP, TCP_PORT)
+    main(TCP_IP, TCP_PORT)
